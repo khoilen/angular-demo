792b8c666518c0f86b3e8fd052d4040734e5117c
khoinashtech <khoilen@nashtechglobal.com>
2025-09-07 16:36:01 +0700

    feat: add about us and contact page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,17 @@
 import { Routes } from '@angular/router';
 import { MainLayout } from './layout/main-layout/main-layout';
 import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+import { Contact } from './pages/contact/contact';
 
 export const routes: Routes = [
   {
     path: '',
     component: MainLayout,
-    children: [{ path: '', component: Home }],
+    children: [
+      { path: '', component: Home },
+      { path: 'about', component: About },
+      { path: 'contact', component: Contact },
+    ],
   },
 ];
