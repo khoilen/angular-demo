913d08bde8b5c81e165361da5299c7874d55c53d
khoinashtech <khoilen@nashtechglobal.com>
2025-09-07 07:41:02 +0700

    feat: update logo and title

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -3,10 +3,15 @@ import { Banner } from '../../components/banner/banner';
 import { SpecialtyMenu } from '../../components/specialty-menu/specialty-menu';
 import { Header } from '../../components/header/header';
 import { TopDoctors } from '../../components/top-doctors/top-doctors';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
   imports: [Banner, SpecialtyMenu, Header, TopDoctors],
   templateUrl: './home.html',
 })
-export class Home {}
+export class Home {
+  constructor(private title: Title) {
+    this.title.setTitle('Home - Doctor Booking App');
+  }
+}
