9d65275504ef77ae6198b2508797f898f4f6f66b
khoinashtech <khoilen@nashtechglobal.com>
2025-09-20 21:05:24 +0700

    update icon

--- src/app/layout/main-layout/main-layout.ts
+++ src/app/layout/main-layout/main-layout.ts
@@ -2,10 +2,15 @@ import { Component } from '@angular/core';
 import { Navbar } from '../navbar/navbar';
 import { Footer } from '../footer/footer';
 import { RouterOutlet } from '@angular/router';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-main-layout',
   imports: [Navbar, Footer, RouterOutlet],
   templateUrl: './main-layout.html',
 })
-export class MainLayout {}
+export class MainLayout {
+  constructor(private title: Title) {
+    this.title.setTitle('Home - Doctor Booking App');
+  }
+}
