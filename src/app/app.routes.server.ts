950a6b008a122ac787ddcb012b6a5bfe3022e3e5
khoinashtech <khoilen@nashtechglobal.com>
2025-09-21 10:30:35 +0700

    fix: build;

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,26 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
+import doctors from './data/doctors.json';
+import { Doctor } from './types/doctor';
 
 export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'appointment/:docId',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      return (doctors as Doctor[]).map((doc) => ({ docId: doc._id }));
+    },
+  },
+  {
+    path: 'doctors/:speciality',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      return (doctors as Doctor[]).map((doc) => ({
+        speciality: doc.speciality.toLocaleLowerCase(),
+      }));
+    },
+  },
 ];
